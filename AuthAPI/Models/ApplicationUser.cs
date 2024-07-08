c67b163f95a98a32a5d9b71feb2933e2abbd4191
homayungit <homayunjust@gmail.com>
2024-07-08 10:53:07 +0600

    Commit All

--- AuthAPI/Models/ApplicationUser.cs
+++ AuthAPI/Models/ApplicationUser.cs
@@ -4,7 +4,7 @@ namespace AuthAPI.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string Name { get; set; }
-        public string EmpCode { get; set; }
+        public string Name { get; set; } = default!;
+        public string EmpCode { get; set; } = default!;
     }
 }
--- AuthAPI/Models/Dto/RegisterationRequestDto.cs
+++ AuthAPI/Models/Dto/RegisterationRequestDto.cs
@@ -2,11 +2,11 @@
 {
     public class RegistrationRequestDto
     {
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public string EmpCode { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Password { get; set; }
+        public string Email { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string EmpCode { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public string Password { get; set; } = default!;
         public string? Role { get; set; }
     }
 }
