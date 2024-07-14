0732c27f1bb9f446a598d025569647f72cc518dd
SAZZAD-HT <sazzadaiub1@gmail.com>
2024-07-14 09:42:34 +0600

    adeda

--- AuthAPI/Data/AppDbContext.cs
+++ AuthAPI/Data/AppDbContext.cs
@@ -13,6 +13,7 @@ namespace AuthAPI.Data
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
