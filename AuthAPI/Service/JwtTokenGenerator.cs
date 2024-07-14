e94480b7ea4f6ae1bba22537a935c01ed3f35eca
SAZZAD-HT <sazzadaiub1@gmail.com>
2024-07-14 10:23:41 +0600

    GenerateToken Null Handle

--- AuthAPI/Service/JwtTokenGenerator.cs
+++ AuthAPI/Service/JwtTokenGenerator.cs
@@ -24,9 +24,9 @@ namespace AuthAPI.Service
 
             var claimList = new List<Claim>
             {
-                new Claim(JwtRegisteredClaimNames.Email,applicationUser.Email),
+                new Claim(JwtRegisteredClaimNames.Email,applicationUser.Email ?? ""),
                 new Claim(JwtRegisteredClaimNames.Sub,applicationUser.Id),
-                new Claim(JwtRegisteredClaimNames.Name,applicationUser.UserName),
+                new Claim(JwtRegisteredClaimNames.Name,applicationUser.UserName ?? ""),
                 new Claim("EmpCode",applicationUser.EmpCode),
                 new Claim("UserId",applicationUser.Id)
             };
